2498cee6a3474e21d33267c07e55d1cdf36c5c3e
Hosam Sultan <hosam5553@gmail.com>
2024-02-14 10:10:59 +0000

    fully implemented

--- src/components/Snippet/Snippet.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Snippet() {
-  return <div></div>;
-}
-
-export default Snippet;
--- src/components/SnippetWrapper/SnippetWrapper.js
+++ src/components/SnippetWrapper/SnippetWrapper.js
@@ -1,7 +1,26 @@
+'use client';
+
 import React from 'react';
 
-function SnippetWrapper() {
-  return <div></div>;
+function SnippetWrapper({children}) {
+  const [
+    visible,
+    setVisible,
+  ] = React.useState(false);
+
+  return (
+    <>
+    {visible ? children: <div className="reveal">
+          <button
+            onClick={() =>
+              setVisible(true)
+            }
+          >
+            Reveal Content
+          </button>
+        </div>}
+    </>
+  );
 }
 
 export default SnippetWrapper;
